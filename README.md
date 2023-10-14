6de3f2d660fe70dbb076292feedf65fa129fd525
rishi-e-kumar <>
2023-10-13 17:28:52 -0700

    updates
    updatess

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# pJV
-code to run pseudoJV scans in the FRG
+# pseudo-JV
+Control and analysis code for pseudo-JV scans in SERF.\
+Directory pJV_analysis_example contains a notebook for pseudo-JV analysis and necessary demo data files.\
+Demo_figures includes an example of the figures that the notebook outputs.\
+![](Demo_figures/Final_plot.png)
---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name="pseudo_JV",
+    packages=['pseudo_JV'],
+    version="1.0",
+    license="",
+    description="Control Code for the pseudo JV setup in the FRG",
+    author="Henry Vossler",
+    author_email="hmvossler@gmail.com",
+    url="https://github.com/fenning-research-group/pseudo_JV",
+    download_url="",
+    keywords=[
+        "Photoluminescence",
+    ],
+    install_requires=[
+    ]
+)
