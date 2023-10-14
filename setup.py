9d8e3d69f9f22ae58a0abd30dcc222761bbac3f0
rishi-e-kumar <>
2023-10-13 19:00:52 -0700

    change 4
    change 4

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name="pseudo_JV",
-    packages=['pseudo_JV'],
+    name="pJV",
+    packages=['pJV'],
     version="1.0",
     license="",
     description="Control Code for the pseudo JV setup in the FRG",
     author="Henry Vossler",
     author_email="hmvossler@gmail.com",
-    url="https://github.com/fenning-research-group/pseudo_JV",
+    url="https://github.com/fenning-research-group/pJV",
     download_url="",
     keywords=[
         "Photoluminescence",
